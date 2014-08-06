edc8bf3085d3ed10dbc09de4bf1aeae5d2952ef4
Robert Pohl <robban.p@gmail.com>
2014-08-06 15:03:24 +0200

    upd tests for refunds

---
+++ MondidoSDK-Test/RefundTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Security.Policy;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Filters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MondidoSDK.Api;
+using MondidoSDK.Configuration;
+using MondidoSDK.Utils;
+
+namespace MondidoSDK_Test
+{
+    [TestClass]
+    public class RefundTest : TestBase
+    {
+
+        [ClassInitialize()]
+        public static void MyClassInitialize(TestContext testContext) 
+        {
+           TestBase.MyClassInitialize(testContext);
+        }
+
+        [TestInitialize()]
+        public void Init()
+        {
+            TestBase.MyClassInitialize(null);
+        }
+
+        [TestMethod]
+        public void TestGet()
+        {
+            var res = Refund.Get(1);
+            Assert.AreEqual(1, res.Id);
+        }
+
+        [TestMethod]
+        public void TestCreate()
+        {
+            var refdata = DateTimeOffset.Now.Ticks.ToString();
+            var postData = new List<KeyValuePair<string, string>>();
+            postData.Add(new KeyValuePair<string, string>("transaction_id", "1"));
+            postData.Add(new KeyValuePair<string, string>("amount", "1.00"));
+            postData.Add(new KeyValuePair<string, string>("reason", refdata));
+            postData.Add(new KeyValuePair<string, string>("locale", "en"));
+
+            var res = MondidoSDK.Api.Refund.Create(postData);
+            Assert.IsTrue(res.Reason == refdata);
+        }
+
+        [TestMethod]
+        public void TestDelete()
+        {
+            var res = StoredCard.Delete(1);
+            Assert.AreEqual("deleted", res.Status);
+        }
+
+
+        [ClassCleanup()]
+        public static void MyClassCleanup()
+        {
+            TestBase.MyClassCleanup();
+        }
+    }
+}
---
+++ MondidoSDK-Test/RefundsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using MondidoSDK.Api;
+using Newtonsoft.Json.Linq;
+
+namespace MondidoSDK_Test
+{
+    public class RefundsController : ApiController
+    {
+        public Refund TestRefund
+        {
+            get
+            {
+                return new Refund()
+                {
+                    Amount = "10.00",
+                    CreatedAt = DateTime.UtcNow,
+                    Reason = "test refund"
+                };
+            }
+        }
+        public object Post()
+        {
+            dynamic obj =  Request.Content.ReadAsAsync<JObject>().Result;
+            var re = TestRefund;
+            re.Amount = obj.amount;
+            re.Reason = obj.reason;
+            re.TransactionId = obj.transaction_id;
+            re.Id = 100;
+            
+            return re;
+        }
+
+        public object Get(int id)
+        {
+            var re = TestRefund;
+            re.Id = id;
+            return re;
+        }
+    }
+}
