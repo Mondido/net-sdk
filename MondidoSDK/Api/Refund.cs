f4b7cda75a60b3d40f23a6ba469dbb9c1dea8ecb
Robert Pohl <robban.p@gmail.com>
2014-08-06 15:03:11 +0200

    upd tests for refunds

--- MondidoSDK/Api/Refund.cs
+++ MondidoSDK/Api/Refund.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using MondidoSDK.Utils;
 using Newtonsoft.Json;
 
 namespace MondidoSDK.Api
 {
-    public class Refund
+    public class Refund : BaseModel
     {
         [JsonProperty(PropertyName = "transaction_id")]
         public int TransactionId { get; set; }
@@ -18,5 +20,19 @@ namespace MondidoSDK.Api
 
         [JsonProperty(PropertyName = "created_at")]
         public DateTime CreatedAt { get; set; }
+
+        [JsonProperty(PropertyName = "id")]
+        public int Id { get; set; }
+
+
+        public static Refund Create(List<KeyValuePair<string, string>> data)
+        {
+            return HttpPost("/refunds", data).Result.FromJson<Refund>();
+        }
+
+        public static Refund Get(int id)
+        {
+            return HttpGet("/refunds/" + id).Result.FromJson<Refund>();
+        }
     }
 }
